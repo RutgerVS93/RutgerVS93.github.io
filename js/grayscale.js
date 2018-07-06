662c9f584d9a20161af2c40c7e86780c02d4a84e
DESKTOP-FM7RFTU\R van Schoonderwalt <rutgerino@hotmail.com>
2018-07-06 14:26:03 +0200

    ewqewq

--- js/grayscale.js
+++ js/grayscale.js
@@ -83,7 +83,7 @@ function loadDoc(){
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function(){
     if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("AJAX").innerHTML = this.responseText;
+      document.getElementById("AJAX1").innerHTML = this.responseText;
       //readXML(this);
     }
   };
