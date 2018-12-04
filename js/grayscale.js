31f97231998c98d1a1ae49b3529ce8f5ac564a56
RutgerVS93 <Rutger.van.Schoonderwalt@gmail.com>
2018-12-04 15:56:27 +0100

    hope

--- js/grayscale.js
+++ js/grayscale.js
@@ -98,13 +98,13 @@ function getLocation(){
     document.getElementById("locationValues").innerHTML = "Geolocation not supported in this browser";
   }
 
-  var mymap = L.map('mapid').setView([51, 0], 13);
+  var mymap = L.map('mapid').setView([51.505, -0.09], 13);
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox.streets',
     accessToken: 'your.mapbox.access.token'
-}).addTo(mymap);
+  }).addTo(mymap);
 }
 
 function showPosition(position){
